31a8b93a37e36ebe6fac9c22421a1340b809cee6
yasu0276 <yoshida37350526@gmail.com>
2024-09-21 22:30:49 +0900

    1. ファイルのドラッグ&ドロップに対応 2. フォーカスリングの無効化に対応

--- src/main.js
+++ src/main.js
@@ -1,13 +1,48 @@
 // Tauri の invoke 関数を使用して Rust コマンドを呼び出します
 const { invoke } = window.__TAURI__.tauri;
+const tauri = window.__TAURI__;
 let currentFilePath = null;  // 保存されたファイルパスを保持する変数
 
-async function openFile() {
+// ページロード時にテキストエリアにフォーカスを当てる
+window.onload = () => {
+  document.getElementById("editor").focus();
+};
+
+// ドラッグイベント検知
+editor.addEventListener('dragover', (event) => {
+  event.preventDefault(); // デフォルトの挙動を防ぐ
+  requestAnimationFrame(() => {
+    // 次のフレームで画面を更新
+    editor.style.cursor = 'move'; // カーソルをドラッグ中の状態に変更
+  });
+});
+
+// Rust 側のドロップイベント検知 ( 本来は javascript 側を使うべきだが Rust 側でないとイベント検知不可のため )
+tauri.event.listen("tauri://file-drop", (event) => {
+  const paths = event.payload;
+  const editor = document.getElementById("editor");
+  if (paths) { 
+    openFile(paths[0]);
+  }
+  requestAnimationFrame(() => {
+    // ドロップ後に描画更新
+    editor.style.cursor = 'text'; // 通常のカーソルに戻す
+    editor.setSelectionRange(0, 0);
+  });
+});
+
+// 非同期ファイルオープン処理を呼び出す
+async function openFile(path) {
+  const content = await invoke("open_file", { path });
+  const editor = document.getElementById("editor");
+  editor.value = content;
+  currentFilePath = path;  // ファイルを開いたらパスを記憶
+}
+
+async function openFileDialog() {
   const path = await window.__TAURI__.dialog.open();
   if (path) {
-    const content = await invoke("open_file", { path });
-    document.getElementById("editor").value = content;
-    currentFilePath = path;  // ファイルを開いたらパスを記憶
+    openFile(path);
   }
 }
 
@@ -39,6 +74,6 @@ document.addEventListener("keydown", (event) => {
 document.addEventListener("keydown", (event) => {
   if ((event.ctrlKey || event.metaKey) && event.key === "o") {
     event.preventDefault(); // ブラウザのデフォルトの動作を防ぐ
-    openFile(); // ファイルを開く関数を呼び出す
+    openFileDialog(); // ファイルを開く関数を呼び出す
   }
 });
