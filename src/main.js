c3c7d36ffc4a56661b7dfa7e5730402745f771b7
Yasu Yoshida <w.yasu.yoshida@gmail.com>
2024-10-10 09:45:47 +0900

    1. 初回起動時にEnter入力が効かない不具合を修正

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 // Tauri の invoke 関数を使用して Rust コマンドを呼び出します
 const { invoke } = window.__TAURI__.tauri;
 const tauri = window.__TAURI__;
-const editor = document.getElementById('editor'); // editor のオブジェクト
+const editor = document.querySelector('.editor'); // editor のオブジェクト
 let currentFilePath = null;  // 保存されたファイルパスを保持する変数
 let isComposing = false;
-
-// ページロード時にテキストエリアにフォーカスを当てる
-window.onload = () => {
-  editor.focus();
-};
 
 function visualizeSpaces(text) {
   return text
@@ -81,9 +76,9 @@ function placeCaretAtEnd() {
   const range = document.createRange();
   range.selectNodeContents(editor);
   range.collapse(false);
-  const sel = window.getSelection();
-  sel.removeAllRanges();
-  sel.addRange(range);
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
 }
 
 // ドラッグイベント検知
@@ -113,61 +108,70 @@ editor.addEventListener('input', () => {
   }
 });
 
-editor.addEventListener("keydown", (event) => {
+// ページロード時にテキストエリアにフォーカスを当てる
+document.addEventListener('DOMContentLoaded', () => {
+  // 初期化
+  editor.focus();
+  editor.innerHTML = '<br>';
+  placeCaretAtEnd();
+  updateLineNumbers();
+
+
   const lineNumbers = document.getElementById('line-numbers');
-  // フォントの拡大
-  if ((event.ctrlKey || event.metaKey) && (event.key === "=" || event.key === "+")) {
-    // 現在のフォントサイズを取得
-    const style = window.getComputedStyle(editor);
-    let fontSize = parseFloat(style.fontSize); // フォントサイズを数値に変換
-    event.preventDefault(); // ブラウザのデフォルトの保存を無効化
-    fontSize += 1; // フォントサイズを変更
-    editor.style.fontSize = fontSize + "px";
-    lineNumbers.style.fontSize = fontSize + "px";
-  }
-  // フォントの縮小
-  if ((event.ctrlKey || event.metaKey) && event.key === "-") {
-    // 現在のフォントサイズを取得
-    const style = window.getComputedStyle(editor);
-    let fontSize = parseFloat(style.fontSize); // フォントサイズを数値に変換
-    event.preventDefault(); // ブラウザのデフォルトの保存を無効化
-    fontSize -= 1; // フォントサイズを変更
-    if (fontSize < 5) {
-      fontSize = 5;
+  editor.addEventListener("keydown", (event) => {
+    // フォントの拡大
+    if ((event.ctrlKey || event.metaKey) && (event.key === "=" || event.key === "+")) {
+      // 現在のフォントサイズを取得
+      const style = window.getComputedStyle(editor);
+      let fontSize = parseFloat(style.fontSize); // フォントサイズを数値に変換
+      event.preventDefault(); // ブラウザのデフォルトの保存を無効化
+      fontSize += 1; // フォントサイズを変更
+      editor.style.fontSize = fontSize + "px";
+      lineNumbers.style.fontSize = fontSize + "px";
     }
-    editor.style.fontSize = fontSize + "px";
-    lineNumbers.style.fontSize = fontSize + "px";
-  }
-  // ファイルオープンのためのファイルダイアログを開く
-  if ((event.ctrlKey || event.metaKey) && event.key === "o") {
-    event.preventDefault(); // ブラウザのデフォルトの動作を防ぐ
-    openFileDialog(); // ファイルを開く関数を呼び出す
-  }
-  // ファイルセーブのためのファイルダイアログを開く
-  if ((event.ctrlKey || event.metaKey) && event.key === "s") {
-    event.preventDefault(); // ブラウザのデフォルトの保存を無効化
-    saveFile(); // ファイル保存関数を呼び出す
-  }
-  // Enter 入力動作を上書き
-  if (event.key === 'Enter') {
-    event.preventDefault(); // デフォルトのEnter動作を無効化
-    const selection = window.getSelection();
-    const range = selection.getRangeAt(0);
+    // フォントの縮小
+    if ((event.ctrlKey || event.metaKey) && event.key === "-") {
+      // 現在のフォントサイズを取得
+      const style = window.getComputedStyle(editor);
+      let fontSize = parseFloat(style.fontSize); // フォントサイズを数値に変換
+      event.preventDefault(); // ブラウザのデフォルトの保存を無効化
+      fontSize -= 1; // フォントサイズを変更
+      if (fontSize < 5) {
+        fontSize = 5;
+      }
+      editor.style.fontSize = fontSize + "px";
+      lineNumbers.style.fontSize = fontSize + "px";
+    }
+    // ファイルオープンのためのファイルダイアログを開く
+    if ((event.ctrlKey || event.metaKey) && event.key === "o") {
+      event.preventDefault(); // ブラウザのデフォルトの動作を防ぐ
+      openFileDialog(); // ファイルを開く関数を呼び出す
+    }
+    // ファイルセーブのためのファイルダイアログを開く
+    if ((event.ctrlKey || event.metaKey) && event.key === "s") {
+      event.preventDefault(); // ブラウザのデフォルトの保存を無効化
+      saveFile(); // ファイル保存関数を呼び出す
+    }
+    // Enter 入力動作を上書き
+    if (event.key === 'Enter') {
+      event.preventDefault();  // デフォルトの改行動作を抑制
 
-    // <br> タグを作成して挿入
-    const br = document.createElement('br');
-    range.insertNode(br);
+      // 改行の挿入
+      const selection = window.getSelection();
+      const range = selection.getRangeAt(0);
 
-    // 改行後にカーソルを移動
-    range.setStartAfter(br);
-    range.setEndAfter(br);
-    selection.removeAllRanges();
-    selection.addRange(range);
+      const br = document.createElement('br');
+      range.deleteContents();  // 現在の選択を削除（もしあれば）
+      range.insertNode(br);  // 改行を挿入
 
-    updateLineNumbers();
-    placeCaretAtEnd();
-  }
+      // 改行後にカーソルをその次の位置に移動させる
+      range.setStartAfter(br);
+      range.setEndAfter(br);
+      selection.removeAllRanges();
+      selection.addRange(range);
+
+      updateLineNumbers();
+    }
+  });
+
 });
-
-// 初期化
-updateLineNumbers();
